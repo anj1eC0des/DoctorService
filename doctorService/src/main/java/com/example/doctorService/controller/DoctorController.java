631dc6fc25cf16fe4183afdcdc5d955df5a896c2
Shubhanjan <rockymountainsandbeaches@gmail.com>
2025-06-12 23:53:54 +0530

    Add placeholder methods and components to fill out logic later

--- doctorService/src/main/java/com/example/doctorService/controller/DoctorController.java
+++ doctorService/src/main/java/com/example/doctorService/controller/DoctorController.java
@@ -53,6 +53,4 @@ public class DoctorController {
         return new ArrayList<Doctor>();
     }
 
-
-
 }
--- doctorService/src/main/java/com/example/doctorService/entity/Doctor.java
+++ doctorService/src/main/java/com/example/doctorService/entity/Doctor.java
@@ -1,10 +1,14 @@
 package com.example.doctorService.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="doctors")
 public class Doctor {
 
