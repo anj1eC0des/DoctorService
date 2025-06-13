d372f22076646022541501db2d7b79a542976eb9
anjanc0des <129589930+anj1eC0des@users.noreply.github.com>
2025-06-13 09:21:31 +0000

    add partial jpa logic

--- doctorService/src/main/java/com/example/doctorService/repository/DoctorRepository.java
+++ doctorService/src/main/java/com/example/doctorService/repository/DoctorRepository.java
@@ -1,12 +1,19 @@
 package com.example.doctorService.repository;
 
+import java.util.List;
 
-import com.example.doctorService.entity.Doctor;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.doctorService.entity.Doctor;
+
 @Repository
-public interface DoctorRepository extends CrudRepository<Doctor ,Integer> {
-    //add custom queries for querying by department and specialisation
-    //check how to sync ids while using different dbs
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+
+    @Query("SELECT d FROM Doctor d WHERE d.specialization LIKE :specialization")
+    public List<Doctor> findBySpecialization(String specialization);
+
+    @Query("SELECT d FROM Doctor d WHERE d.departmentId = :departmentId")
+    public List<Doctor> findByDepartment(int departmentId);
 }
--- doctorService/src/main/java/com/example/doctorService/service/DoctorService.java
+++ doctorService/src/main/java/com/example/doctorService/service/DoctorService.java
@@ -1,42 +1,61 @@
 package com.example.doctorService.service;
 
-import com.example.doctorService.entity.Doctor;
-import com.example.doctorService.entity.DoctorDTO;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.doctorService.entity.Doctor;
+import com.example.doctorService.entity.DoctorDTO;
+import com.example.doctorService.repository.DoctorRepository;
 
 @Service
 public class DoctorService {
-    public Doctor creatDoctor(DoctorDTO patient){
-        //jpa save - repo.save(patient)
-        return new Doctor();
+
+    private final DoctorRepository doctorRepository;
+
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
     }
 
-    public List<Doctor> listDoctors(){
-        // repo.listDoctors;
-        return new ArrayList<>();
+    public Doctor creatDoctor(DoctorDTO doctor) {
+        Doctor doc = new Doctor();
+        doc.setName(doctor.getName());
+        doc.setSpecialization(doctor.getSpecialization());
+        doc.setDepartmentId(doctor.getDepartmentId());
+        doc.setContactInformation(doctor.getContactInformation());
+        return doctorRepository.save(doc);
     }
 
-    public Doctor getDoctor(int id){
-        //patient find by id
-        return new Doctor();
+    public List<Doctor> listDoctors() {
+        return doctorRepository.findAll();
     }
 
-    public Doctor updateDoctor(int id, DoctorDTO patient){
-        return new Doctor();
+    public Optional<Doctor> getDoctor(int id) {
+
+        return doctorRepository.findById(id);
     }
 
-    public int deleteDoctor(int id){
-        return 0;
+    public Doctor updateDoctor(int id, DoctorDTO doctor) {
+        Doctor doc = new Doctor();
+        doc.setDoctorId(id);
+        doc.setName(doctor.getName());
+        doc.setSpecialization(doctor.getSpecialization());
+        doc.setDepartmentId(doctor.getDepartmentId());
+        doc.setContactInformation(doctor.getContactInformation());
+        return doctorRepository.save(doc);
     }
 
-    public List<Doctor> searchDoctorsBySpecialization(String name){
-        return new ArrayList<Doctor>();
+    public void deleteDoctor(int id) {
+        doctorRepository.deleteById(id);
     }
 
-    public List<Doctor> searchDoctorByDepartment(int departmentId){
-        return new ArrayList<Doctor>();
+    public List<Doctor> searchDoctorsBySpecialization(String name) {
+        return doctorRepository.findBySpecialization(name);
+    }
+
+    public List<Doctor> searchDoctorByDepartment(int departmentId) {
+        return doctorRepository.findByDepartment(departmentId);
     }
 }
