4274d4555b47ddf821b4fcfb7a05f5c5a8ac3d98
anjanc0des <129589930+anj1eC0des@users.noreply.github.com>
2025-06-13 10:53:58 +0000

    add files

--- doctorService/src/main/java/com/example/doctorService/app/DoctorServiceApplication.java
+++ doctorService/src/main/java/com/example/doctorService/app/DoctorServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.doctorService.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example.doctorService")
+@EntityScan("com.example.doctorService")
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
